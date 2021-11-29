c2f304daaa9e3ce707b45bd9fd85cd98fb0c3d19
ahnsoheee <soheeeahn@gmail.com>
2021-11-30 00:04:13 +0900

    다차원배열

---
+++ Chapter5/3_MultiDemensionalArray.md
@@ -0,0 +1,20 @@
+## 다차원 배열
+
+### 배열 생성
+
+```java
+int [][] score = new int[4][3];
+```
+
+### 가변 배열
+- 다차원 배열을 생성할 때 전체 배열 차수 중 마지막 차수의 길이를 지정하지 않고, 추후에 각기 다른 배열을 생성함으로써 보다 유동적인 가변 배열로 구성할 수 있다.
+
+```java
+int [][] score = new int[5][];
+score[0] = new int[4];
+score[1] = new int[2];
+score[2] = new int[3];
+score[3] = new int[3];
+score[4] = new int[2];
+
+```
---
+++ Chapter5/ArrayEx18.java
@@ -0,0 +1,21 @@
+package Chapter5;
+
+public class ArrayEx18 {
+    public static void main(String[] args) {
+        int[][] score = {
+                { 100, 100, 100 },
+                { 20, 20, 20 },
+                { 30, 30, 30 },
+                { 40, 40, 40 }
+        };
+
+        int sum = 0;
+
+        for (int[] tmp : score) {
+            for (int i : tmp) {
+                sum += i;
+            }
+        }
+        System.out.println("sum = " + sum);
+    }
+}
